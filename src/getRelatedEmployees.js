41a826b8968c6eee00f92a832279e2149f8788c2
gus-aguiar <gustavopma90@gmail.com>
2022-10-31 19:33:36 -0300

    fiz a 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const gerenciadas = [];
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  if (isManager(managerId) === true) {
+    const nomeSobre = [];
+    employees.forEach((element) => {
+      if (element.managers.includes(managerId)) {
+        nomeSobre.push(`${element.firstName} ${element.lastName}`);
+      }
+    });
+    return nomeSobre;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
