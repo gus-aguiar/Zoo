72387fcb43c5b193ae5902f8ef402581c3ae0267
gus-aguiar <gustavopma90@gmail.com>
2022-10-31 17:53:48 -0300

    fiz a 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find(({ firstName, lastName }) => employeeName === firstName
+  || employeeName === lastName);
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName('Nigel'));
