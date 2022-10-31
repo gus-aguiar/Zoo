e82ef0eb1fa1da2d4ad2f08b5213583d90eb7bc8
gus-aguiar <gustavopma90@gmail.com>
2022-10-31 17:34:58 -0300

    fiz a dois

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  return data.filter((element) => {
-    
-  })
+  const animais = species.find((element) => element.name === animal);
+  return animais.residents.every(((element) => element.age >= age));
 }
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('otters', 7));
