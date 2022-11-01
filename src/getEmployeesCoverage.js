bd5cd2145b1939c0a96219a497d9673ecaf102fd
gus-aguiar <gustavopma90@gmail.com>
2022-11-01 19:10:27 -0300

    fiz o nove dois

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,18 +1,18 @@
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-const { species } = data
-const { employees } = data;
+// const { species } = data
+// const { employees } = data;
 
-function getEmployeesCoverage(idOuNome) {
-  const empregadoNome = employees.find((element) => element.firstName === idOuNome.name);
-  const nomeCompleto = `${empregadoNome.firstName} ${empregadoNome.lastName}`;
-  const ident = empregadoNome.id;
-  const speciesID = empregadoNome.responsibleFor
-  const getSpecies = (...speciesID) => species.find((element) => element.id === speciesID)  
-  return { id: ident, fullName: nomeCompleto, species: teste};
-}
+// function getEmployeesCoverage(idOuNome) {
+//   const empregadoNome = employees.find((element) => element.firstName === idOuNome.name);
+//   const nomeCompleto = `${empregadoNome.firstName} ${empregadoNome.lastName}`;
+//   const ident = empregadoNome.id;
+//   const speciesID = empregadoNome.responsibleFor
+//   const getSpecies = (...speciesID) => species.find((element) => element.id === speciesID)
+//   return { id: ident, fullName: nomeCompleto, species: teste};
+// }
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
-module.exports = getEmployeesCoverage;
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// module.exports = getEmployeesCoverage;
 
-//======================parei, falta retomar====================================//
+// //======================parei, falta retomar====================================//
