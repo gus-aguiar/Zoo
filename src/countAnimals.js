a1b38306e4847b18851b6a215ec2a87b96a68822
gus-aguiar <gustavopma90@gmail.com>
2022-10-31 22:20:29 -0300

    tentando fazer a 5, vou parar por hoje

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const total = {};
+    for (let index = 0; index < species.length; index += 1) {
+      total.push(`${species.name} , ${species.residents.length}`);
+    }
+  }
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals());
