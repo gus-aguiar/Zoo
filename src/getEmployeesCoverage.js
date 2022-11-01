4952becb7a36beec5a42172b122bc05519fb8fca
gus-aguiar <gustavopma90@gmail.com>
2022-11-01 19:06:21 -0300

    fiz o nove

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data
+const { employees } = data;
+
+function getEmployeesCoverage(idOuNome) {
+  const empregadoNome = employees.find((element) => element.firstName === idOuNome.name);
+  const nomeCompleto = `${empregadoNome.firstName} ${empregadoNome.lastName}`;
+  const ident = empregadoNome.id;
+  const speciesID = empregadoNome.responsibleFor
+  const getSpecies = (...speciesID) => species.find((element) => element.id === speciesID)  
+  return { id: ident, fullName: nomeCompleto, species: teste};
 }
 
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 module.exports = getEmployeesCoverage;
+
+//======================parei, falta retomar====================================//
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,40 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const animaisEmExibicao = (day) =>
+  species.filter((specie) => specie.availability.includes(day)).map((animal) => animal.name);
+
+const daysDaSemana = () => Object.assign(
+  {},
+  ...Object.keys(hours).map((dia) => {
+    const { open, close } = hours[dia];
+    return {
+      [dia]: {
+        officeHour: dia !== 'Monday' ? `Open from ${open}am until ${close}pm` : 'CLOSED',
+        exhibition: dia !== 'Monday' ? animaisEmExibicao(dia) : 'The zoo will be closed!',
+      },
+    };
+  }),
+);
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const verificaEspecie = species.some((obj) => Object.values(obj).includes(scheduleTarget));
+  const resultado = daysDaSemana();
+  const dias = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday', 'Sunday'];
+
+  if (!scheduleTarget) return resultado;
+
+  if (verificaEspecie) {
+    return species.find((element) => element.name === scheduleTarget).availability;
+  }
+
+  if (dias.includes(scheduleTarget)) {
+    return {
+      [scheduleTarget]: resultado[scheduleTarget],
+    };
+  }
+  return resultado;
 }
 
 module.exports = getSchedule;
+
+console.log(daysDaSemana());
