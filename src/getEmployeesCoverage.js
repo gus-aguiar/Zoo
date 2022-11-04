cb41b6a30ddbb6c63ebf1498327edd029c7d2fe2
gus-aguiar <gustavopma90@gmail.com>
2022-11-04 18:12:32 -0300

    resetei a onze, vou tentar refatorar

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -4,16 +4,18 @@ const { species } = data;
 const { employees } = data;
 
 function getEmployeesCoverage(idOuNome) {
-  const pegaAnimal = (id) => species.find((specie) => specie.id === id).name;
-  const pegaLocation = (id) => species.find((specie) => specie.id === id).location;
-  const empregadoNome = employees.find((element) => element.firstName === idOuNome.name);
-  const nomeCompleto = `${empregadoNome.firstName} ${empregadoNome.lastName}`;
-  const ident = empregadoNome.id;
-  const speciesID = empregadoNome.responsibleFor.map((element) => pegaAnimal(element));
-  const speciesLocation = empregadoNome.responsibleFor.map((element) => pegaLocation(element));
-  if (Object.values(employees).includes(idOuNome.name)) {
-    return { id: ident, fullName: nomeCompleto, species: speciesID, locations: speciesLocation };
-  }
+
 }
 console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 module.exports = getEmployeesCoverage;
+
+// function getEmployeesCoverage(idOuNome) {
+//   const pegaAnimal = (id) => species.find((specie) => specie.id === id).name;
+//   const pegaLocation = (id) => species.find((specie) => specie.id === id).location;
+//   const empregadoNome = employees.find((element) => element.firstName);
+//   const nomeCompleto = `${empregadoNome.firstName} ${empregadoNome.lastName}`;
+//   const ident = empregadoNome.id;
+//   const speciesID = empregadoNome.responsibleFor.map((element) => pegaAnimal(element));
+//   const speciesLocation = empregadoNome.responsibleFor.map((element) => pegaLocation(element));
+//   return { id: ident, fullName: nomeCompleto, species: speciesID, locations: speciesLocation };
+// }
